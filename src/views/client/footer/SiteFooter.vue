c0d4b97ae670ab9c02248c084a94e1ee060607c8
SeanBecker <rabbitwerks.js@gmail.com>
2019-06-15 15:07:50 -0500

    update: site footer bg image adjusted

--- src/views/client/footer/SiteFooter.vue
+++ src/views/client/footer/SiteFooter.vue
@@ -1,23 +1,33 @@
 <template>
   <div class="site-footer--outer fbx fd-c sp-ctr">
-    <span class="site-footer--text">&copy;2019 DJ Encouter | All Rights Reserved</span>
-
-    <a href="https://www.youtube.com/rabbitwerksjavascript" class="credit">rabbitwerks.js</a>
+    <span class="site-footer--text">&copy;{{ brandText }}</span>
+    <a :href="credit.link" class="credit">{{ credit.text }}</a>
   </div>
 </template>
 
 <script>
-
-
 export default {
-
+  data() {
+    return {
+      brandText: '2019 DJ Encouter | All Rights Reserved',
+      credit: {
+        text: 'rabbitwerks.js',
+        link: 'https://www.youtube.com/rabbitwerksjavascript',
+      }
+    }
+  }
 }
 </script>
 
 <style scoped>
 .site-footer--outer {
   min-height: 20vh;
-  background: url('../../../assets/img/view-landing_space01-1_1920x1080.jpg') rgba(0, 0, 0, 1);
+  background-image: url('../../../assets/img/view-landing_space01-1_1920x1080.jpg');
+  background-position: 0 20%;
+  padding-top: 1.5rem;
+}
+.background-image {
+  height: 100px;
 }
 .site-footer--text {
   letter-spacing: 2px;
