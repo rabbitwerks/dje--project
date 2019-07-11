7c65d0a9994611e6e570c48d39ca3ca2bd0ccd77
SeanBecker <rabbitwerks.js@gmail.com>
2019-07-10 20:06:43 -0500

    add: login view, user-bar header cmp, logic for supporting added dmp

---
+++ src/components/reusable/user-bar/UserBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="user-bar--outer fxbx a-ctr">
+    <div class="ub--username">
+      <span>Greetings, {{ name.toUpperCase() }}</span>
+    </div>
+    <div class="ub--settings-outer">
+      <!-- needs to settings cog in svg format ;) -->
+      <img 
+        class="settings-cog pointer"
+        src="../../../assets/img/single_cog_SM.svg" 
+        alt="settings cog svg">
+      <img>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: 'Earthling'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.user-bar--outer {
+  padding-top: .5rem;
+}
+.ub--username {
+  padding-right: 1rem;
+}
+.ub--settings-outer {
+  padding-top: .25rem;
+}
+.settings-cog {
+  width: 1.25rem;
+}
+</style>
--- src/views/admin/Admin.vue
+++ src/views/admin/Admin.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="admin--shell">
-    <h1>Admin Dashboard</h1>
+    <div class="admin-header--outer fxbx spc-bt a-ctr">
+      <h1 class="header--title">Admin Dashboard</h1>
+      <UserBar name="Brady" />
+    </div>
     <div
       :class="viewport.width < 480 ? 'fd-c' : ''"
       class="admin--outer fxbx"
@@ -12,11 +15,13 @@
 </template>
 
 <script>
+import UserBar from '@/components/reusable/user-bar/UserBar.vue'
 import MenuPanel from '@/components/admin/menu-panel/MenuPanel.vue';
 import ControllerPanel from '@/components/admin/control-panel/ControllerPanel.vue';
 
 export default {
   components: {
+    UserBar,
     MenuPanel,
     ControllerPanel,
   },
@@ -34,6 +39,10 @@ export default {
   background-color: var(--view-color--two);
   border: 2px solid #eee;
 }
+.admin-header--outer {
+  padding: 1rem 0;
+}
+
 
 @media screen and (max-width: 480px) {
   .admin--shell {
